723ed716d492e991efdf804913fde72034dd8dab
someshwar reddy <somesh.satish@gmail.com>
2022-06-22 21:55:14 +0530

    js

---
+++ javascript/ex.js
@@ -0,0 +1,48 @@
+var a =10;
+// var b =20; write in the data to the memory
+// let h ;
+const
+h=234234;
+function a() {
+
+}
+
+a()
+{
+    a:'kjdfgkjd'
+}
+[]
+
+// console.log(b); from the memory
+// 1) memory life cycle
+// excution context 
+
+// memoryheap stack callstck 
+
+// 1) stack 
+// allocates memory for static data  (primative data types) fixed
+
+const a=null;
+const b= 'kggdfhgdhgd';
+const d = 123;
+
+// stack wili happen 
+
+// const d= 123;
+// const b= 'kggdfhgdhgd;
+// const a=null;
+// refernce for that particular non primative datatypes
+
+// memoryheap
+// allocates memory for dynamic  data  (non primative data types) fixed
+
+const ad = {
+    a:'kfgjdksghgh',
+    b:24224234
+} 
+
+// memory heap 
+// {
+//     a:'kfgjdksghgh',
+//     b:'{}'
+// } 
